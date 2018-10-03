7c0525f265bd960f1f5e76ea1d7f419a8865528b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-10-03 22:21:50 +0200

    Added demo

---
+++ demo/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>RectangularMozaic demo</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,400" />
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body id="home">
+    <h1>RectangularMozaic demo</h1>
+    <a href=".">
+        <?php include 'table.php' ?>
+    </a>
+</body>
+</html>
---
+++ demo/table.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use \RectangularMozaic\Generator as Mozaic;
+use \RectangularMozaic\Cell;
+
+$tilesNumber = 20;
+$columns = 5;
+
+$grid = Mozaic::generate($tilesNumber, $columns);
+
+echo '<table>';
+$i = 1;
+
+foreach ($grid->getValues() as $row) {
+    echo '<tr>';
+    foreach ($row as $cell) {
+        switch ($cell) {
+            case Cell::SMALL:
+                echo '<td>' . $i . '</td>';
+                $i += 1;
+                break;
+            case Cell::TALL_TOP:
+                echo '<td rowspan="2">' . $i . '</td>';
+                $i += 1;
+                break;
+            case Cell::TALL_BOTTOM:
+                break; // do nothing
+            case Cell::WIDE_LEFT:
+                echo '<td colspan="2">' . $i . '</td>';
+                $i += 1;
+                break;
+            case Cell::WIDE_RIGHT:
+                break; // do nothing
+        }
+    }
+    echo '</tr>';
+}
+echo '</table>';
